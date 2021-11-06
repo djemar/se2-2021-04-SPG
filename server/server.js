f59882c76a0e141ef95e6739c07f18d6b0f207d2
LeSimo <cv.sim96@gmail.com>
2021-11-06 16:01:23 +0100

    Server Setup
    First Server Commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan"); // logging middleware
+const jwt = require("express-jwt");
+const jsonwebtoken = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { check, validationResult } = require("express-validator"); // validation library
+//const dao = require("./dao.js");
+
+const passport = require("passport"); // auth middleware
+const LocalStrategy = require("passport-local").Strategy; // username and password for login
+const session = require("express-session"); // enable sessions
+
+const app = express();
+const port = 3001;
+
+// Set-up logging
+app.use(morgan("tiny"));
+
+// Process body content
+app.use(express.json());
+
+// DB error
+const dbErrorObj = { errors: [{ param: "Server", msg: "Database error" }] };
+// Authorization error
+const authErrorObj = {
+    errors: [{ param: "Server", msg: "Authorization error" }],
+};
+
+const expireTime = 300; //seconds
+
+app.listen(port, () =>
+    console.log(`Server app listening at http://localhost:${port}`)
+);
