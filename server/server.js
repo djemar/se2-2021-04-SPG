08874b6b0a27d176e4ab64340d671a9bed48f0cb
Diego Marino <marinodiego.96@gmail.com>
2021-11-28 18:54:49 +0100

    fix redirect

--- server/server.js
+++ server/server.js
@@ -418,7 +418,7 @@ app.post(
 );
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(CLIENT_BUILD_PATH, "index.html"));
 });
 
 app.listen(port, HOST, () =>
